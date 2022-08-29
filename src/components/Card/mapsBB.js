694dcaaf694574fdb64c439e3776c51dfcf6f968
Aryanda Rizky P <aryandarizky1234@gmail.com>
2022-08-29 23:37:34 +0700

    add new map

--- src/components/Card/mapsBB.js
+++ src/components/Card/mapsBB.js
@@ -1,14 +1,8 @@
-// import {auth} from '../firebase'
-import { Link, useNavigate } from "react-router-dom";
-// import Highcharts from "highcharts";
 import * as Highcharts from "highcharts/highmaps";
 
 import React, { useEffect, useState } from "react";
-import { ref, set, child, get, update, remove } from "firebase/database";
-import { db, auth } from "../../firebase";
-import GrafikBBLakiLaki from "./garfikBBLaki";
 
-const Maps = () => {
+const MapsObesitas = () => {
   let [fetchStatus, setFetchStatus] = useState(true);
 
   useEffect(() => {
@@ -22,50 +16,50 @@ const Maps = () => {
       // data and map.
       const data = [
         ["id-3700", 10],
-        ["id-ac", 2.2],
-        ["id-jt", 1.1],
-        ["id-be", 0.2],
-        ["id-bt", 1.4],
-        ["id-kb", 1.8],
-        ["id-bb", 0.1],
-        ["id-ba", 0.4],
-        ["id-ji", 1.9],
-        ["id-ks", 1.4],
-        ["id-nt", 1.3],
-        ["id-se", 0.7],
-        ["id-kr", 0.9],
-        ["id-ib", 2.9],
-        ["id-su", 0.6],
-        ["id-ri", 0.9],
-        ["id-sw", 0.4],
-        ["id-ku", 1.6],
-        ["id-la", 1.2],
-        ["id-sb", 1.2],
-        ["id-ma", 1.2],
-        ["id-nb", 0.9],
-        ["id-sg", 1.2],
-        ["id-st", 1.3],
-        ["id-pa", 2.8],
-        ["id-jr", 0.8],
-        ["id-ki", 1.6],
-        ["id-1024", 0.9],
-        ["id-jk", 0.7],
-        ["id-go", 1.1],
-        ["id-yo", 0.6],
-        ["id-sl", 1.1],
-        ["id-sr", 1.2],
-        ["id-ja", 0.5],
-        ["id-kt", 1.8],
+        ["id-ac", 0.8], //aceh
+        ["id-jt", 1.5], //jateng
+        ["id-be", 0.5], //bengkulu
+        ["id-bt", 1.3], //banten
+        ["id-kb", 1.1], //kalbar
+        ["id-bb", 1.3], //babel
+        ["id-ba", 1.8], //bali
+        ["id-ji", 2.1], //jatim
+        ["id-ks", 1.0], //kalsel
+        ["id-nt", 0.4], //ntt
+        ["id-se", 0.5], //sulsel
+        ["id-kr", 1.4], //kep ri
+        ["id-ib", 1.3], // papua barat
+        ["id-su", 0.7], //sumut
+        ["id-ri", 0.6], //riau
+        ["id-sw", 0.6], //sulawesi utara
+        ["id-ku", 1.3], //kalimantan utara
+        ["id-la", 0.4], //maluku utara
+        ["id-sb", 0.6], // sumbar
+        ["id-ma", 0.4], //maluku
+        ["id-nb", 0.8], //ntb
+        ["id-sg", 0.6], // sulawesi tenggara
+        ["id-st", 0.8], //sulawesi tengah
+        ["id-pa", 1.4], // papua
+        ["id-jr", 1.2], //jabar
+        ["id-ki", 1.9], //kaltim
+        ["id-1024", 0.7], //lampung
+        ["id-jk", 1.6], //dki jkrta
+        ["id-go", 0.6], //gorontalo
+        ["id-yo", 2.2], //yogya
+        ["id-sl", 0.9], //sumsel
+        ["id-sr", 0.7], //sulawesi barat
+        ["id-ja", 0.4], //jambi
+        ["id-kt", 1.5], //kalteng
       ];
 
       // Create the chart
-      Highcharts.mapChart("mapGiziContainer", {
+      Highcharts.mapChart("mapObesitasContainer", {
         chart: {
           map: topology,
         },
 
         title: {
-          text: "Persebaran gizi buruk indonesia",
+          text: "Persebaran obesitas indonesia",
         },
 
         subtitle: {
@@ -114,9 +108,9 @@ const Maps = () => {
 
   return (
     <>
-      <div id="mapGiziContainer"></div>
+      <div id="mapObesitasContainer"></div>
     </>
   );
 };
 
-export default Maps;
+export default MapsObesitas;
--- src/components/Pages/home.js
+++ src/components/Pages/home.js
@@ -4,6 +4,7 @@ import GrafikTBLakiLaki from "../Card/grafikTBLaki";
 import RiwayatPengukuranCard from "../Card/riwayatPengukuranCard";
 import Highcharts from "highcharts";
 import Maps from "../Card/maps";
+import MapsObesitas from "../Card/mapsBB";
 
 const Home = () => {
   return (
@@ -11,22 +12,7 @@ const Home = () => {
       <div className="dashboardContainer">
         <h1>Selamat Datang</h1>
         <Maps />
-        {/* <div className="homeContainer"> */}
-        <div className="flex flex-wrap">
-          <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4"></div>
-          <div className="w-full xl:w-4/12 px-4">
-            <GrafikTBLakiLaki />
-          </div>
-        </div>
-        <div className="flex flex-wrap mt-4">
-          <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-            <RiwayatPengukuranCard />
-          </div>
-          <div className="w-full xl:w-4/12 px-4">
-            <GrafikIMTLakiLaki />
-          </div>
-        </div>
-        {/* </div> */}
+        <MapsObesitas />
       </div>
     </>
   );
